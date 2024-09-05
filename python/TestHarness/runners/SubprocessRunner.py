loganharbour/moose

Call correct member

python/TestHarness/runners/SubprocessRunner.py
@@ -59,7 +59,7 @@ class SubprocessRunner(Runner):
         # Set this for OpenMPI so that we don't clobber state
         if tester.hasOpenMPI():
             popen_env = os.environ.copy()
-            popen_env['OMPI_MCA_orte_tmpdir_base'] = self.getTempDirectory().name
+            popen_env['OMPI_MCA_orte_tmpdir_base'] = self.getTester().getTempDirectory().name
             process_kwargs['env'] = popen_env
 
         try:

2lambda123/idaholab-moose

python/TestHarness/runners/SubprocessRunner.py
@@ -61,7 +61,7 @@ class SubprocessRunner(Runner):
             process_env = os.environ.copy()
 
             # Don't clobber state
-            process_env['OMPI_MCA_orte_tmpdir_base'] = self.getTempDirectory().name
+            process_env['OMPI_MCA_orte_tmpdir_base'] = self.getTester().getTempDirectory().name
             # Allow oversubscription for hosts that don't have a hostfile
             process_env['PRTE_MCA_rmaps_default_mapping_policy'] = ':oversubscribe'
 