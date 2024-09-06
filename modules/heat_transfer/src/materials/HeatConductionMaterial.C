d7ac20ab05e0455f172da3ddae60dd856bb9f80b
Guillaume Giudicelli <guillaume.giudicelli@inl.gov>
2024-09-05 18:06:13 -0600

    Use solution invalid warning for repeated and tallied warning in HC material, closes #17047

--- modules/heat_transfer/src/materials/HeatConductionMaterial.C
+++ modules/heat_transfer/src/materials/HeatConductionMaterial.C
@@ -91,13 +91,8 @@ HeatConductionMaterialTempl<is_ad>::computeQpProperties()
   {
     if (_temperature[_qp] < *_min_T)
     {
-      mooseDoOnce(mooseWarning("Temperature (",
-                               _temperature[_qp],
-                               ") is below min_T (",
-                               *_min_T,
-                               ") at ",
-                               _q_point[_qp],
-                               ". \nUsing min_T instead of temperature."));
+      flagSolutionInvalidWarning("Temperature below specified minimum (" + std::to_string(*min_T) +
+                                 "). min_T will be used instead.");
       qp_temperature = *_min_T;
     }
   }
